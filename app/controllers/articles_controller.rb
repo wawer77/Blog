5768955368e083045ccb0a31a67457eca01a44e9
wawer77 <tomek.06@wp.pl>
2018-07-09 00:30:44 +0200

    Add edit, update, delete methods; add/amend views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     @article.save
@@ -18,7 +22,17 @@ class ArticlesController < ApplicationController
     #if function for saving or not to be added
   end
 
+  def update
+    @article=Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to @article
+  end
 
+  def destroy
+    @article=Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+    end
 
   #PRIVATE methods
   private
